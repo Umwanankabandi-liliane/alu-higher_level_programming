d60565745b068127c09140a73fb5169ed1d0ab2e
Umwanankabandi-liliane <l.umwananka1@alustudent.com>
2024-02-23 12:05:35 -0800

    Task 3

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,7 +1,17 @@
 #!/usr/bin/python3
+def add_arg(argv):
+    n = len(argv) - 1
+    if n == 0:
+        print("{:d}".format(n))
+        return
+    else:
+        i = 1
+        add = 0
+        while i <= n:
+            add += int(argv[i])
+            i += 1
+        print("{:d}".format(add))
+
 if __name__ == "__main__":
-    from sys import argv
-    sumint = 0
-    for i in range(1, len(argv)
-        sumint += int(argv[i])
-    print("{}".format(sumint))    
+    import sys
+    add_arg(sys.argv)
