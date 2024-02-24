e8bd93e5a387816152f193dbd4add16b607d3475
Umwanankabandi-liliane <l.umwananka1@alustudent.com>
2024-02-24 04:53:23 -0800

    Task 15

--- python-if_else_loops_functions/101-remove_char_at.py
+++ python-if_else_loops_functions/101-remove_char_at.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-def remove_char_at(str, n):
-    new = ' '
-    i = 0
-    for char in str:
-        if i != n:
-            new += str[i]
-        i += 1
-    return (new)    
+#Author - bamidele Adefolaju
+
+def remove_char_at(str , n):
+    if n < 0:
+        return (str)
+    return (str[:n] + str[n+1:])
