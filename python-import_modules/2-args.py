5a98aeed292bd03610d9738d6c36c1a75326f242
Umwanankabandi-liliane <l.umwananka1@alustudent.com>
2024-02-25 05:31:14 -0800

    Task 2

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -15,4 +15,4 @@ if __name__ == "__main__":
 
     else:
         print("{} argument:".format(size))
-        print("{}: {}".format(size, arg[1]))        
+        print("{}: {}".format(size, arg[1]))
