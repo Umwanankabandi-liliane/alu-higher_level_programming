1bc083c051d2d8c2031bd902d91f572fdb07ca2c
Umwanankabandi-liliane <l.umwananka1@alustudent.com>
2024-02-26 03:04:59 -0800

    Task 4

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from hidden_4.pyc import *
+    from hidden_4 import *
     allf = dir()
     for i in range(0, len(allf)):
         if allf[i][:2] != "__":
