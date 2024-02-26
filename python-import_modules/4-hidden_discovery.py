7d0c21aa55902cae84578cdc1375b11d49599db1
Umwanankabandi-liliane <l.umwananka1@alustudent.com>
2024-02-26 03:02:58 -0800

    Task 4

--- python-import_modules/4-hidden_discovery.py
+++ python-import_modules/4-hidden_discovery.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import hidden_4.pyc
-    # print sorted name from directory
-    for name in sorted(dir(hidden_4)):
-        #print only names that do not start with __
-        if name[2:] != '__':
-            print("{}".format(name))
+    from hidden_4.pyc import *
+    allf = dir()
+    for i in range(0, len(allf)):
+        if allf[i][:2] != "__":
+            print("{:s}".format(allf[i]))
