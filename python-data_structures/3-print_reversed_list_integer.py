8028004e0513cf72c57c2c236ec88589181ac8ac
Umwanankabandi-liliane <l.umwananka1@alustudent.com>
2024-02-26 03:44:29 -0800

    Task 3

--- python-data_structures/3-print_reversed_list_integer.py
+++ python-data_structures/3-print_reversed_list_integer.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    from sys import argv
-    sumint = 0
-    for i in range(1, len(argv)):
-        sumint += int(argv[i])
-    print("{}".format(sumint))    
+def print_reversed_list_integer(my_list=[]):
+    if my_list:
+        for elm in my_list[::-1]:
+            print("{:d}".format(elim))
