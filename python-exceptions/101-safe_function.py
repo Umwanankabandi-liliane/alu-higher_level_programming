86e7d7842c9acbd3c8710f464bf4d7b412f836bb
Umwanankabandi-liliane <l.umwananka1@alustudent.com>
2024-02-27 02:39:17 -0800

    Task 8

--- python-exceptions/101-safe_function.py
+++ python-exceptions/101-safe_function.py
@@ -9,4 +9,3 @@ def safe_function(fct, *args):
     except (ValueError, ZeroDivisionError, TypeError, IndexError):
         print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
         return None
-
