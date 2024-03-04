f21cf097e902a18bc84ff945dac9805543e2bd32
Umwanankabandi-liliane <l.umwananka1@alustudent.com>
2024-03-04 11:05:08 -0800

    Task 9

--- python-classes/102-square.py
+++ python-classes/102-square.py
@@ -26,7 +26,7 @@ class Square:
         elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
-    
+
     def area(self):
         """Return the current area of the square."""
         return (self.__size * self.__size)
@@ -38,7 +38,7 @@ class Square:
     def __ne__(self, other):
         """Define the != comparison to a Square."""
         return self.area() != other.area()
-    
+
     def __lt__(self, other):
         """Define the < comparison to a Square."""
         return self.area() < other.area()
