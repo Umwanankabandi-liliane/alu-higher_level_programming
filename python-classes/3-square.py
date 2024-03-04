225b0a48a37ec568431a6cf80912a5a07fd44590
Umwanankabandi-liliane <l.umwananka1@alustudent.com>
2024-03-04 01:17:45 -0800

    Task 3

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -10,13 +10,13 @@ class Square:
         """Initialize a new square.
 
         Args:
-            size (Init): The size of the new square.
+            size (int): The size of the new square.
         """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        selif.__size = size
+        self.__size = size
      
     def area(self):
         """Return the current area of the square."""
