cdf210a0960cd7bfcafe422766d5b50541e43650
Umwanankabandi-liliane <l.umwananka1@alustudent.com>
2024-03-05 03:53:28 -0800

    Task 6

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -13,11 +13,11 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
-
         Args:
             width (int): The width of the new rectangle.
             height (int): The height of the new rectangle.
         """
+        type(self).number_of_instances += 1
         self.width = width
         self.height = height
 
@@ -29,7 +29,6 @@ class Rectangle:
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
-
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -53,7 +52,6 @@ class Rectangle:
         return (self.__width * self.__height)
 
     def perimeter(self):
-
         """Return the perimeter of the Rectangle."""
         if self.__width == 0 or self.__height == 0:
             return (0)
