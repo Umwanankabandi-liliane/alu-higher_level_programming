61933e4a6a563a785622c055872258e013e868db
Umwanankabandi-liliane <l.umwananka1@alustudent.com>
2024-03-09 04:38:33 -0800

    Task 3

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
-def write_file(filename="", text=""):
-    try:
-        with open(filename, 'w', encoding='utf-8') as file:
-            file.write(text)
-            return len(text)
-    except Exception as e:
-        print(f"An error occurred: {e}")
-        return 0
+"""Defined a string-to-JSON function."""
+import json
+
+
+def to_json_string(my_obj):
+    """Return the JSON representation of a string object."""
+    return json.dumps(my_obj)
