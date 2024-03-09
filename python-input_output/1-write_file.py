292f541b1d771804838bb66ed6cc1d4f684755b6
Umwanankabandi-liliane <l.umwananka1@alustudent.com>
2024-03-08 23:07:19 -0800

    Task 1

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Defines a text file line-counting function."""
+
+
+def write_file(filename="", text=""):
+    """Return the number of lines in a text file."""
+    lines = 0
+    with open(filename) as f:
+        for line in f:
+            lines += 1
+    return        
