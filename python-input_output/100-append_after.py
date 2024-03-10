3caa505a8e2e081b2b904df9a574c80ff177fd36
Umwanankabandi-liliane <l.umwananka1@alustudent.com>
2024-03-10 01:50:15 -0800

    Task 11

--- python-input_output/100-append_after.py
+++ python-input_output/100-append_after.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-"""Defines a file-appending function."""
+"""Defines a text file insertion function."""
 
 
-def append_write(filename="", search_string="", new_string=""):
+def append_after(filename="", search_string="", new_string=""):
     """Insert text after each line containing a given string in a file.
     Args:
         filename (str): The name of the file.
