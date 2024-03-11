80aee37e474666d0e5431b6f5be81b0a6550cb37
Umwanankabandi-liliane <l.umwananka1@alustudent.com>
2024-03-11 07:17:36 -0700

    Task 7

--- python-classes/100-singly_linked_list.py
+++ python-classes/100-singly_linked_list.py
@@ -1,52 +1,56 @@
 #!/usr/bin/python3
-
-"""Define classes for a singly-linked list."""
-
-
 class Node:
-    """Represent a node in a singly-linked list."""
-
     def __init__(self, data, next_node=None):
-        """Initialize a new Node.
-
-        Args:
-            data (int): The data of the new Node.
-            next_node (Node): The next node of the new Node.
-        """
         self.data = data
         self.next_node = next_node
 
     @property
     def data(self):
-        """Get/set the data of the Node."""
-        return (self.__data)
+        return self.__data
 
     @data.setter
     def data(self, value):
         if not isinstance(value, int):
-            raise TypeError("data must be an integer")
+            raise TypeError('data must be an integer')
         self.__data = value
 
     @property
     def next_node(self):
-        """Get/set the next_node of the Node."""
-        return (self.__next_node)
+        return self.__next_node
 
     @next_node.setter
     def next_node(self, value):
-        if not isinstance(value, Node) and value is not None:
-            raise TypeError("next_node must be a Node object")
+        if value is not None and type(value) != Node:
+            raise TypeError('next_node must be a Node object')
         self.__next_node = value
 
-class SinglyLinkedList:
-    """Represent a singly-linked list."""
 
+class SinglyLinkedList:
     def __init__(self):
-        """Initialize a new SinglyLinkedList."""
-        self.__head = None
+        self.head = None
+
+    def __str__(self):
+        ret = ""
+        node = self.head
+        while node:
+            ret += str(node.data) + "\n"
+            node = node.next_node
+        return ret[:-1]
 
     def sorted_insert(self, value):
-        """Insert a new node into the correct sorted position in 
-        increasing order."""
-        new_node = Node(value)
+        new = Node(value)
+        if not self.head:
+            self.head = new
+            return
 
+        if value < self.head.data:
+            new.next_node = self.head
+            self.head = new
+            return
+
+        node = self.head
+        while node.next_node and node.next_node.data < value:
+            node = node.next_node
+        if node.next_node:
+            new.next_node = node.next_node
+        node.next_node = new
