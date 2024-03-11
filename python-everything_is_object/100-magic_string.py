0b460a7522301618a0d97f4c7ff24f0a70e57731
Umwanankabandi-liliane <l.umwananka1@alustudent.com>
2024-03-11 09:05:30 -0700

    Task 30

--- python-everything_is_object/100-magic_string.py
+++ python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string():
-    setattr(magic_string, "x", getattr(magic_string, "x", -1) + 1)
-    return "BestSchool" + ", BestSchool" * getattr(magic_string, "x", 0)
+def magic_string(H=[]):
+    H += ["BestSchool"]
+    return (", ".join(H)
