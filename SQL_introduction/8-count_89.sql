6c502382682ff871e7e73bd8ae5ea0f0e27ec32a
Umwanankabandi-liliane <l.umwananka1@alustudent.com>
2024-03-18 05:51:55 -0700

    Task 7, 8

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,2 +1,4 @@
--- Inserts a new row into the table first_table.
-INSERT INTO `first_table` (`id`, `name`) VALUES (89, "Holberton School");
+-- Displays the number of records with id = 89 in the table first_table.
+SELECT COUNT(*)
+FROM `first_table`
+WHERE `id` = 89;
