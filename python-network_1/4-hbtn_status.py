42f1ebf2408e6292ddbc99f74ab973a2a04a9dee
Umwanankabandi-liliane <l.umwananka1@alustudent.com>
2024-03-24 11:45:38 +0000

    Task 4

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
-"""fetches https://intranet.hbtn.io/status using requests"""
+"""
+takes in a URL, sends a request to the URL and displays the
+value of the X-Request-Id variable found in the header of the
+response using requests
+"""
 import requests
+from sys import argv
 
 
 if __name__ == "__main__":
-    """fetches https://intranet.hbtn.io/status using requests"""
-    r = requests.get('https://intranet.hbtn.io/status')
-    print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+    """takes in a URL, sends a request to the URL and displays
+    the value of the X-Request-Id variable found in the header
+    of the response using requests"""
+    r = requests.get(argv[1])
+    try:
+        r_id = r.headers['X-Request-Id']
+        print(r_id)
+    except:
+        pass
