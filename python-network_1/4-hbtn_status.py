76716f8aa27013485e2a8f162f847e6f49b28fa0
Umwanankabandi-liliane <l.umwananka1@alustudent.com>
2024-03-24 21:24:32 +0000

    Task 4

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -4,10 +4,11 @@ Python script that fetches https://alu-intranet.hbtn.io/status
 """
 import requests
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     url = 'https://intranet.hbtn.io/status'
     if url.startswith('https://'):
         url = "https://alu-intranet.hbtn.io/status"
+
     res = requests.get(url)
     print("Body response:")
     print("\t- type: {}".format(type(res.text)))
